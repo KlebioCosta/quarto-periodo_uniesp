870af1181ee8eb02f2468ad2950d2a9a2457d8d6
Edinailton Klébio C  Ferreira <112285772+KlebioCosta@users.noreply.github.com>
2024-03-19 13:25:45 -0300

    exercicios de sala

---
+++ tec-front-end_3__kelson/atividades/workspace_front-end/front-end/atividades-react/atividade03/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RenderinzandoComFuncoes from './components/RenderinzandoComFuncoes'
+
+const App = () => {
+  return (
+    <div>
+      <RenderinzandoComFuncoes />
+    </div>
+  )
+  
+}
+
+export default App
